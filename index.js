edb9c7f257e5ba462c7740790e8d34a4c8e6070b
Ben Anthony <ben.anthony@flatironschool.com>
2026-02-04 18:24:30 -0700

    create index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+// FUNCTION passFail(score)
+//     IF score is greater than or equal to 50
+//         PRINT "Pass"
+//     ELSE
+//         PRINT "Fail"
+// END FUNCTION
+
+function passFail(score) {
+    let result;
+}
